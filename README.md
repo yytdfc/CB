84e9e291261e32d4755dab0c08b9f09a0f7f2673
FuChen <fuchen@foxmail.com>
2024-02-21 17:06:02 +0800

    first init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Amazon Bedrock nodes for ComfyUI
+
+Amazon Bedrock is a fully managed service that offers a choice of high-performing foundation models (FMs) from leading AI companies.
+This repo is the ComfyUI nodes for Bedrock service. You could invoke the foundation model in your ComfyUI pipeline.
+
+## Installation
+Clone this repository to your ComfyUI `custom_nodes` directory:
+``` bash
+cd ComfyUI/custom_nodes
+git clone https://github.com/yytdfc/ComfyUI-Bedrock.git
+```
+
+
+## Example
+
+For example, you could use the Bedrock LLM to refine the prompt input and get a better result. Here is an example of doing prompt translation and refinement, and the invoke the image generation model (eg. SDXL, TitanImage) provided by Bedrock.
+The result is much better after preprocessing of prompt compared to the original SDXL model (bottom output), which doesn't have the capability of understanding Chinese. Workflow examples are in `./workflows`.
+![](./assets/example.webp)
+
+
+
+## Support models:
+
+Here are models ready for use, more models are coming soon.
+
+- Anthropic:
+    - [x] Claude (1.x, 2.0, 2.1)
+    - [x] Claude Instant (1.x)
+
+- Amazon:
+    - [x] Titan Image Generator G1 (1.x)
+
+- Stability AI:
+    - [x] Stable Diffusion XL (1.0)
+
---
+++ nodes/bedrock_base.py
@@ -0,0 +1,373 @@
+import json
+import os
+import random
+import time
+import re
+import base64
+from io import BytesIO
+
+from PIL import Image
+import numpy as np
+import torch
+
+import boto3
+
+os.environ["HTTP_PROXY"] = ""
+os.environ["ALL_PROXY"] = ""
+
+MAX_RETRY = 3
+
+bedrock_runtime_client = boto3.client(service_name='bedrock-runtime')
+
+
+class BedrockClaude:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "prompt": ("STRING", {"multiline": True}),
+                "is_raw_prompt": ([
+                    False,
+                    True, 
+                ],),
+                "model_id": ([
+                    "anthropic.claude-v2:1", 
+                    "anthropic.claude-v2",
+                    "anthropic.claude-v1",
+                    "anthropic.claude-instant-v1",
+                ],),
+                "max_tokens_to_sample": ("INT", {
+                    "default": 200, 
+                    "min": 0, #Minimum value
+                    "max": 4096, #Maximum value
+                    "step": 1, #Slider's step
+                    "display": "number" # Cosmetic only: display as "number" or "slider"
+                }),
+                "temperature": ("FLOAT", {
+                    "default": 0.5,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.1,
+                    "round": 0.001, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
+                    "display": "slider"
+                }),
+                "top_p": ("FLOAT", {
+                    "default": 1.0,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.1,
+                    "round": 0.001, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
+                    "display": "slider"
+                }),
+                "top_k": ("INT", {
+                    "default": 250,
+                    "min": 0,
+                    "max": 500,
+                    "step": 1,
+                    "round": 1, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
+                    "display": "slider"
+                }),
+            }
+        }
+    
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "forward"
+    CATEGORY = "aws"
+
+    def forward(self, prompt, is_raw_prompt, model_id, max_tokens_to_sample, temperature, top_p, top_k):
+        """
+        Invokes the Anthropic Claude 2 model to run an inference using the input
+        provided in the request body.
+
+        :param prompt: The prompt that you want Claude to complete.
+        :return: Inference response from the model.
+        """
+        if not is_raw_prompt:
+            enclosed_prompt = "Human: " + prompt + "\n\nAssistant:"
+        elif not prompt.strip().startswith("Human: "):
+            enclosed_prompt = "Human: " + prompt + "\n\nAssistant:"
+        else:
+            enclosed_prompt = prompt
+
+        for _ in range(MAX_RETRY):
+            try:
+                # The different model providers have individual request and response formats.
+                # For the format, ranges, and default values for Anthropic Claude, refer to:
+                # https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-claude.html
+
+                # Claude requires you to enclose the prompt as follows:
+
+                body = {
+                    "prompt": enclosed_prompt,
+                    "max_tokens_to_sample": max_tokens_to_sample,
+                    "temperature": temperature,
+                    "top_p": top_p,
+                    "top_k": top_k,
+                    # "stop_sequences": ["\n\nHuman:"],
+                }
+
+                response = bedrock_runtime_client.invoke_model(
+                    modelId=model_id, body=json.dumps(body, ensure_ascii=False)
+                )
+
+                response_body = json.loads(response["body"].read())
+                completion = response_body["completion"]
+
+                return (completion, )
+
+            except Exception as e:
+                print(e)
+        else:
+            raise RuntimeError("Couldn't invoke Anthropic Claude")
+
+
+class BedrockTitanImage:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "prompt": ("STRING", {"multiline": True}),
+                "num_images": ("INT", {
+                    "default": 1,
+                    "min": 1,
+                    "max": 5,
+                    "step": 1,
+                    "round": 1, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
+                    "display": "number"
+                }),
+                "quality": ([
+                    "standard",
+                    "premium",
+                ],),
+                "resolution": ([
+                    "1024 x 1024",
+                    "768 x 768",
+                    "512 x 512",
+                    "768 x 1152",
+                    "384 x 576",
+                    "1152 x 768",
+                    "576 x 384",
+                    "768 x 1280",
+                    "384 x 640",
+                    "1280 x 768",
+                    "640 x 384",
+                    "896 x 1152",
+                    "448 x 576",
+                    "1152 x 896",
+                    "576 x 448",
+                    "768 x 1408",
+                    "384 x 704",
+                    "1408 x 768",
+                    "704 x 384",
+                    "640 x 1408",
+                    "320 x 704",
+                    "1408 x 640",
+                    "704 x 320",
+                    "1152 x 640",
+                    "1173 x 640",
+                ],),
+                "cfg_scale": ("FLOAT", {
+                    "default": 7.0,
+                    "min": 0.0,
+                    "max": 35.0,
+                    "step": 0.1,
+                    "round": 0.001, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
+                    "display": "slider"
+                }),
+                "seed": ("INT", {
+                    "default": 0,
+                    "min": 0,
+                    "max": 2147483646,
+                    "step": 1,
+                    "round": 1, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
+                    "display": "number"
+                }),
+            }
+        }
+    
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "forward"
+    CATEGORY = "aws"
+
+    def forward(self, prompt, num_images, quality, resolution, cfg_scale, seed):
+        """
+        Invokes the Titan Image model to create an image using the input provided in the request body.
+
+        :param prompt: The prompt that you want Amazon Titan to use for image generation.
+        :param seed: Random noise seed (range: 0 to 2147483647)
+        :return: Base64-encoded inference response from the model.
+        """
+
+        height, width = map(int, re.findall(r'\d+', resolution))
+
+        for _ in range(MAX_RETRY):
+            try:
+                # The different model providers have individual request and response formats.
+                # For the format, ranges, and default values for Titan Image models refer to:
+                # https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-titan-image.html
+
+                request = json.dumps(
+                    {
+                        "taskType": "TEXT_IMAGE",
+                        "textToImageParams": {"text": prompt},
+                        "imageGenerationConfig": {
+                            "numberOfImages": num_images,
+                            "quality": quality,
+                            "cfgScale": cfg_scale,
+                            "height": height,
+                            "width": width,
+                            "seed": seed,
+                        },
+                    },
+                    ensure_ascii=False,
+                )
+
+                response = bedrock_runtime_client.invoke_model(
+                    modelId="amazon.titan-image-generator-v1", body=request
+                )
+
+                response_body = json.loads(response["body"].read())
+                # base64_image_data = response_body["images"][0]
+                images = [
+                    np.array(Image.open(BytesIO(base64.b64decode(base64_image))))
+                    for base64_image in response_body.get("images")
+                ]
+                images = [
+                    torch.from_numpy(np.array(image).astype(np.float32) / 255.0)
+                    for image in images
+                ]
+                return (images,)
+
+            except Exception as e:
+                print(e)
+        else:
+            raise RuntimeError("Couldn't invoke Anthropic Claude")
+
+
+class BedrockSDXL:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "prompt": ("STRING", {"multiline": True}),
+                "resolution": ([
+                    "1024 x 1024",
+                    "1152 x 896",
+                    "1216 x 832",
+                    "1344 x 768",
+                    "1536 x 640",
+                    "640 x 1536",
+                    "768 x 1344",
+                    "832 x 1216",
+                    "896 x 1152",
+                ],),
+                "style_preset": ([
+                    "None",
+                    "3d-model", "analog-film", "anime", "cinematic", "comic-book", "digital-art", 
+                    "enhance", "fantasy-art", "isometric", "line-art", "low-poly", "modeling-compound", 
+                    "neon-punk", "origami", "photographic", "pixel-art", "tile-texture",
+                ],),
+                "cfg_scale": ("FLOAT", {
+                    "default": 8.0,
+                    "min": 1.1,
+                    "max": 10.0,
+                    "step": 0.1,
+                    "round": 0.001, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
+                    "display": "slider"
+                }),
+                "steps": ("INT", {
+                    "default": 30,
+                    "min": 10,
+                    "max": 50,
+                    "step": 1,
+                    "round": 1, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
+                    "display": "number"
+                }),
+                "clip_guidance_preset": ([
+                    "NONE", "FAST_BLUE", "FAST_GREEN", "SIMPLE", 
+                    "SLOW", "SLOWER", "SLOWEST",
+                ],),
+                "sampler": ([
+                    "Auto",
+                    "DDIM",
+                    "DDPM",
+                    "K_DPMPP_2M",
+                    "K_DPMPP_2S_ANCESTRAL",
+                    "K_DPM_2",
+                    "K_DPM_2_ANCESTRAL",
+                    "K_EULER",
+                    "K_EULER_ANCESTRAL",
+                    "K_HEUN",
+                    "K_LMS",
+                ],),
+                "seed": ("INT", {
+                    "default": 0,
+                    "min": 0,
+                    "max": 4294967295,
+                    "step": 1,
+                    "round": 1, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
+                    "display": "number"
+                }),
+                        
+
+
+            }
+        }
+    
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "forward"
+    CATEGORY = "aws"
+
+    def forward(self, prompt, resolution, style_preset, cfg_scale, steps, clip_guidance_preset, sampler, seed):
+
+        height, width = map(int, re.findall(r'\d+', resolution))
+        print(steps, height, width, cfg_scale, clip_guidance_preset, seed, style_preset, sampler)
+
+        for _ in range(MAX_RETRY):
+            try:
+                # The different model providers have individual request and response formats.
+                # For the format, ranges, and available style_presets of Stable Diffusion models refer to:
+                # https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-stability-diffusion.html
+
+                request = {
+                    "text_prompts": [{"text": prompt}],
+                    "seed": seed,
+                    "cfg_scale": cfg_scale,
+                    "steps": steps,
+                    "height": height,
+                    "width": width,
+                    "clip_guidance_preset": clip_guidance_preset,
+                    "seed": seed,
+                }
+                if style_preset != "None":
+                    request["style_prompts"] = style_preset
+                if sampler != "Auto":
+                    request["sampler"] = sampler
+
+                response = bedrock_runtime_client.invoke_model(
+                    modelId="stability.stable-diffusion-xl-v1", body=json.dumps(request, ensure_ascii=False)
+                )
+
+                response_body = json.loads(response["body"].read())
+                images = [
+                    np.array(Image.open(BytesIO(base64.b64decode(base64_image["base64"]))))
+                    for base64_image in response_body.get("artifacts")
+                ]
+                images = [
+                    torch.from_numpy(np.array(image).astype(np.float32) / 255.0)
+                    for image in images
+                ]
+                return (images,)
+
+            except Exception as e:
+                print(e)
+        else:
+            raise RuntimeError("Couldn't invoke Anthropic Claude")
+
+
+NODE_CLASS_MAPPINGS = {
+    "Bedrock - Claude": BedrockClaude,
+    "Bedrock - Titan Image": BedrockTitanImage,
+    "Bedrock - SDXL": BedrockSDXL,
+}
+print(NODE_CLASS_MAPPINGS)
